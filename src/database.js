861ec475aa2b5a80823f02aa57c1cef8d39057e4
VibergStefan <viberg.stefan@outlook.com>
2025-06-17 20:33:47 +0200

    Update and rename database.js to main.tsx

--- src/database.js
+++
@@ -1,5 +0,0 @@
-export default const poleDatabase = [
-  { type: "Trästolpe", height: 12, maxLoad: 500 },
-  { type: "Betongstolpe", height: 15, maxLoad: 800 },
-  { type: "Stålstolpe", height: 20, maxLoad: 1200 }
-];
