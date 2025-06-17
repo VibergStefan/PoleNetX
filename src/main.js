ff97a204f912c2297da39c9857ee706a7693c71f
VibergStefan <viberg.stefan@outlook.com>
2025-06-17 20:08:09 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import initCAD from "./cad-view.js";
 import initGISTools from "./gis-tools.js";
+import L from "leaflet";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const map = L.map("map").setView([57.427, 14.216], 10);
+  
+  L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenTopoMap & OpenStreetMap"
+  }).addTo(map);
+
   document.getElementById("switchToGIS").addEventListener("click", () => switchView("gis"));
   document.getElementById("switchToCAD").addEventListener("click", () => switchView("cad"));
   
